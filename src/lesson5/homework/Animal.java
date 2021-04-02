4521d111fed2107c91638eecbf2926e17d3a65ac
NickPopandopulo <nikas.popandopulos@gmail.com>
2021-04-02 14:02:00 +0700

    .

--- src/lesson5/homework/Animal.java
+++
@@ -1,9 +0,0 @@
-package lesson5.homework;
-
-public abstract class Animal {
-
-    protected abstract void run(double distance);
-    protected abstract void swim(double distance);
-    protected abstract void jump(double distance);
-
-}
