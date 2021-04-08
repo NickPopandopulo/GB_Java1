bfc4f051fa424a8bc34cfc894d1c25bbd672436d
NickPopandopulo <nikas.popandopulos@gmail.com>
2021-04-08 22:50:02 +0700

    Доработаны крестики-нолики

--- src/lesson3/homework/Homework3.java
+++ src/lesson3/homework/Homework3.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 public class Homework3 {
 
     public static char[][] map;
-    public static int sizeMapX;
-    public static int sizeMapY;
+    public static int sizeMapX = 5;
+    public static int sizeMapY = 5;
 
     public static Scanner scanner = new Scanner(System.in);
     public static Random random = new Random();
@@ -26,7 +26,7 @@ public class Homework3 {
     public static int win = 4;
 
     public static void main(String[] args) {
-        createMap(5, 5);
+        createMap(sizeMapX, sizeMapY);
         printMap();
 
         while (true) {
@@ -87,7 +87,7 @@ public class Homework3 {
             x = scanner.nextInt() - 1;
             System.out.print("Введите позицию Y: ");
             y = scanner.nextInt() - 1;
-        } while (!(isValid(x, y) && isEmpty(x, y)));
+        } while (!(isValid(y, x) && isEmpty(y, x)));
 
         map[y][x] = human;
     }
@@ -106,16 +106,16 @@ public class Homework3 {
             // Рандомный поиск ближайшей к человеку свободной клетки для ИИ
             x += random.nextInt(3) - 1;
             y += random.nextInt(3) - 1;
-        } while (!isValid(x, y) || !isEmpty(x, y));
+        } while (!isValid(y, x) || !isEmpty(y, x));
 
         map[y][x] = ai;
     }
 
-    public static boolean isValid(int x, int y) {
+    public static boolean isValid(int y, int x) {
         return x >= 0 && x < sizeMapX && y >= 0 && y < sizeMapY;
     }
 
-    public static boolean isEmpty(int x, int y) {
+    public static boolean isEmpty(int y, int x) {
         return map[y][x] == empty;
     }
 
@@ -129,41 +129,32 @@ public class Homework3 {
     }
 
     public static boolean isWin(char player) {
-        int[] count;
+        int[] count = new int[2];
+        int[] countDiag = new int[2];
 
-        // --- Диагонали ---
-
-        // Ниже главной (и побочной) диагонали
-        count = countDiagonals(1, 0, player);
-        if (count[0] == win || count[1] == win) return true;
-
-        // Главная (и побочная) диагональ
-        count = countDiagonals(0, 0, player);
-        if (count[0] == win || count[1] == win) return true;
-
-        // Выше главной (и побочной) диагонали
-        count = countDiagonals(0, 1, player);
-        if (count[0] == win || count[1] == win) return true;
-
-        // --- Диагонали ---
-
-        // Вертикальные и горизонатльные линии
         for (int i = 0; i < sizeMapY; i++) {
-            count[0] = 0; // вертикальная
-            count[1] = 0; // горизонтальная
+            count[0] = 0;
+            count[1] = 0;
+            countDiag[0] = 0;
+            countDiag[1] = 0;
             for (int j = 0; j < sizeMapX; j++) {
 
+                // Вертикальные и горизонатльные линии
                 count[0] = (map[i][j] == player) ? ++count[0] : 0;
                 count[1] = (map[j][i] == player) ? ++count[1] : 0;
 
-                if (count[0] == win || count[1] == win) return true;
+                // Диагонали
+                countDiag = countDiagonals(i, j, player, win);
+
+                // Если где-то набралось win символов верт. или гориз. подряд, выходим
+                if (count[0] == win || count[1] == win || countDiag[0] == win || countDiag[1] == win) return true;
             }
         }
 
         return false;
     }
 
-    public static int[] countDiagonals(int i, int j, char player) {
+    public static int[] countDiagonals(int i, int j, char player, int win) {
         int[] count = new int[2];
         int x;
         int y;
@@ -172,7 +163,7 @@ public class Homework3 {
             y = k + i;
             x = k + j;
 
-            if (!isValid(x, y)) continue;
+            if (!isValid(y, x)) continue;
 
             count[0] = (map[y][x] == player) ? ++count[0] : 0; // главная дианональ (или возле нее)
             count[1] = (map[y][sizeMapX - x - 1] == player) ? ++count[1] : 0; // побочная
@@ -183,4 +174,35 @@ public class Homework3 {
         return count;
     }
 
+    public static int[] blockCoordinates() {
+        int[] count = new int[2];
+        int[] countDiag = new int[2];
+
+        int aboutToWin = Homework3.win - 1;
+
+        for (int i = 0; i < sizeMapY; i++) {
+            count[0] = 0;
+            count[1] = 0;
+            countDiag[0] = 0;
+            countDiag[1] = 0;
+            for (int j = 0; j < sizeMapX; j++) {
+
+                // Вертикальные и горизонатльные линии
+                count[0] = (map[i][j] == human) ? ++count[0] : 0;
+                if (count[0] == aboutToWin) {
+                    if (isValid(i, j + 1) && isEmpty(i, j + 1)) return new int[]{i, j + 1};
+                    if (isValid(i, j - aboutToWin) && isEmpty(i, j - aboutToWin)) return new int[]{i, j - aboutToWin};
+                }
+                count[1] = (map[j][i] == human) ? ++count[1] : 0;
+
+                // Диагонали
+                countDiag = countDiagonals(i, j, human, aboutToWin);
+
+                // Если где-то набралось win символов верт. или гориз. подряд, выходим
+//                if (count[0] == aboutToWin || count[1] == aboutToWin || countDiag[0] == aboutToWin || countDiag[1] == aboutToWin) return true;
+            }
+        }
+
+        return null;
+    }
 }
